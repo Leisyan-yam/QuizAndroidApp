0ae294528e9a312656495227f8caf8b57c84da2e
Leisyan Yamaltdinova <n.leisyan@yandex.ru>
2022-10-03 04:09:47 +0300

    "first commit"

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,57 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+    public static String question [] ={
+    "1.How long is a marathon distance?",
+    "2.What is the only sport to be played on the moon?",
+    "3.Who won the 2018 FIFA World Cup?",
+    "4.In karate, what color belt comes right before black?",
+    "5.How many days does Wimbledon traditionally last?",
+    "6.What is Usain Bolts' world record time in the 100 meters?",
+    "7.The Olympics are held every how many years?",
+    "8.Which country won the first FIFA World Cup in 1930?",
+    "9.Of all the fighting sports below, which sport wasn’t practiced by Bruce Lee?",
+    "10.What type of race is the Tour de France?",
+    "11.In what year were women allowed to compete in the Olympic games?",
+    "12.What sport do Tiger Woods play?",
+    "13.When is International Yoga Day celebrated?",
+    "14.Which country will host the 2026 Winter Olympics?",
+    "15.When The International Day of Sport for Development and Peace is celebrated?"
+    };
+    public static String choices [][] ={
+    {"26.2 miles -  42.195 kilometres","13.1 miles - 21,097 kilometres  ","19,2 miles  -  30,9 kilometres ","33.2 miles - 53,108 kilometres "},
+    {"Golf","Chess","Football","Boxing"},
+    {"Croatia","France ","England","Italy"},
+    {"Yellow ","Blue","Brown","Green"},
+    {"16 days","10 days","12 days","14 days"},
+    {"8.59 seconds","9.58 seconds","8.47 seconds","9.39 seconds"},
+    {"2 years","6 years","4 years","8 years"},
+    {"Argentina","Uruguay","United States","Yugoslavia"},
+    {"Wushu","Boxing","Jeet Kune Do","Fencing"},
+    {"Drag race","Motocross","Bicycle race","Figure 8 race"},
+    {"1924","1936","1912","1900"},
+    {"Tennis","Golf ","Figure skating","Box"},
+    {"21 June","23 June","21 July","21 August"},
+    {"Portugal","Spain","Italy ","Germany"},
+    {"6 April ","16 April ","4 May","13 April "}
+
+
+    };
+    public static String correctAnswers[] ={
+    "26.2 miles -  42.195 kilometres ",
+    "Golf",
+    "France",
+    "Brown",
+    "14 days",
+    "9.58 seconds",
+    "4 years",
+    "Uruguay",
+    "Wushu",
+    "Bicycle race",
+    "1900",
+    "Golf",
+    "21 June",
+    "Italy",
+    "6 April"
+    };
+}
